72bcc9a6d1f282b33df80037c2995de8e990caf3
tiff <tiffany.nguyen1@optum.com>
2021-01-29 17:18:33 -0500

    adding initial changes

---
+++ components/test-space.js
@@ -0,0 +1,3 @@
+export default function TestSpace() {
+  return <section className="w-screen h-screen"></section>
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Hero from '../components/hero';
+import TestSpace from '../components/test-space';
+
+export default function Home() {
+  return (
+    <>
+      <Hero/>
+      <TestSpace/>
+    </>
+  )
+}
