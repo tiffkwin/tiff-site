ff24320b432af0f1f2b6f498868c36300b4f6c7f
tiffkwin <tiffanyknnc@gmail.com>
2021-03-10 21:08:01 -0500

    changing favicon

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from '../components/hero';
 import About from '../components/about';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head><title>Tiffany K. Nguyen</title></Head>
       <Hero/>
       <About />
     </>
